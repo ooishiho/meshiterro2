529d2493b407b7e25f4491491d329e14b057ecd8
EC2 Default User <ec2-user@ip-172-31-8-23.ap-northeast-1.compute.internal>
2023-03-04 07:18:08 +0000

    Initial Commit

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,41 @@
+class PostImagesController < ApplicationController
+  def new
+    @post_image = PostImage.new
+  end
+ # 投稿データの保存
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+
+  def index
+    @post_images = PostImage.all
+  end
+
+  def show
+    @post_image = PostImage.find(params[:id])
+  end
+
+
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to '/post_images'
+  end
+ # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+end
